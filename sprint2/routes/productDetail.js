400847d120280c6a02d994d6008a1c727010efc6
MicaDG <micaela.8010@gmail.com>
2021-09-03 15:30:01 -0300

    Subo vista versión mobile de Detalle del producto

--- sprint2/routes/productDetail.js
+++ sprint2/routes/productDetail.js
@@ -2,6 +2,6 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
-router.get("/", (req,res) => res.sendFile(path.join(__dirname,"..", "views", "home.html")));
+router.get("/productDetail", (req,res) => res.sendFile(path.join(__dirname,"..", "views", "productDetail.html")));
 
 module.exports = router;
